66ffc049c72f331187a9304685cca88698a4563b
MatiasFontecillaBusch <matias.f.fontecilla@gmail.com>
2026-05-16 14:08:18 -0400

    feat: add simple redirection in login

--- client/src/features/auth/login/login.ts
+++ client/src/features/auth/login/login.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  private router = inject(Router);
 
+  login() {
+    this.router.navigateByUrl('/products');
+  }
 }
